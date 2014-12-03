0b6401a88f76616ee2b47986dcb0338a86650e3a
sathish-igate <sathish.b@igate.com>
2014-12-03 11:44:21 +0530

    03-dec-14

--- charactersunite/settings.local.php
+++ charactersunite/settings.local.php
@@ -1,10 +1,17 @@
 <?php
+
+//ini_set('max_execution_time', 300);
+//ini_set('max_input_time', 180);
+//ini_set('memory_limit', '256M');
+//ini_set('post_max_size', '36M');
+//ini_set('upload_max_filesize', '12M');
+
 // Define the current environment.
 $_ENV['AH_SITE_ENVIRONMENT'] = 'local';
 
 $databases['default'] = array ('default' =>
   array (
-    'database' => 'nbcuunite',  // <--- Typically "publisher7_<sitename>"
+    'database' => 'charactersunite',  // <--- Typically "publisher7_<sitename>"
     'username' => 'root',
     'password' => '',
     'host' => 'localhost',
